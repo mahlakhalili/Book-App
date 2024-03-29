dfcf2c0c587626ff4cef2c997aad63f10cf027fe
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-03-29 01:30:00 -0700

    Update BookCard.jsx

--- src/Component/BookCard.jsx
+++ src/Component/BookCard.jsx
@@ -2,7 +2,7 @@ import React from "react"
 function BookCard({data : {title , author , image , country ,language , link , pages , year}}){
     return(
         <div>
-            <img src={image} alt={title} />
+            <img src={`/src/assets/${image}`} alt={title} />
             <div>
                 <h3>{title}</h3>
                 <p>{author}</p>
