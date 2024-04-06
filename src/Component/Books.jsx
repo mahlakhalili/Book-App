60d33085b844560c7fb6259e7f8b1105afdf106b
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-04-06 04:22:48 -0700

    Update Books.jsx

--- src/Component/Books.jsx
+++ src/Component/Books.jsx
@@ -1,38 +1,50 @@
 import React, { useState } from "react"
 
-
 import {books} from "../constants/mockData";
 import BookCard from "./BookCard"
+import SideCard from "./SideCard";
+
+import styles from "./Books.module.css"
 
 function Books(){
     const [liked,setLiked] = useState([])
 
-    const handleLikedList = (book , status) => {
+    const handleLikedList = (book , status) => 
+    {
+        console.log(liked)
         if(status){
             const newlikedList = liked.filter((i) = i.id != book.id)
             setLiked(newlikedList)
         }
         else{
-                setLiked(...liked,book)
+            setLiked(...liked,book)
+            }
             
-        }
     }
         
-    
     return(
-        <>
-            <div>
-                {books.map((book) => (
-                <BookCard 
-                key={book.id} 
-                data={book} 
-                handleLikedList={handleLikedList}
-                />
-                ))}</div>
+        
+            <div className = {styles.container}>
+                <div className = {styles.cards}>
+                    <h4>FAVORITE</h4>
+                    {books.map((book) => (
+                    <BookCard 
+                    key={book.id} 
+                    data={book} 
+                    handleLikedList={handleLikedList}
+                    />
+                ))}
+                </div>
             {
-                !!liked.length && (<div></div>)
+                !! liked.length && (
+                <div 
+                    className = {styles.favorite}>
+                        {liked.map(book => <SideCard 
+                            key={book.id} 
+                            data={book}/>)}</div>)
             }
-        </>
+            </div>
+       
     )
 }
 
