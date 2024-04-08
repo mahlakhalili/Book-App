6fb934ce222621117e9b41a44066000750a0aa76
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-04-08 04:31:11 -0700

    update book and searchbox

---
+++ src/Component/SearchBox.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { RiSearch2Line } from "react-icons/ri";
+
+function SearchBox({search , setSearch , searchHandler}){
+    return(
+        <div>
+            <input 
+                type = "text" 
+                placeholder = "Search"
+                value = {search}
+                onChange={e => setSearch(e.target,value.toLowerCase().trim)}
+                onClick={searchHandler}
+                />
+            <button><RiSearch2Line /></button>
+        </div>
+    )
+}
+
+export default SearchBox
