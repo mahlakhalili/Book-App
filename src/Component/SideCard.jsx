25296d439d2148923cb7bc05cca71f577da139e4
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-04-08 04:44:33 -0700

    Update SideCard.jsx

--- src/Component/SideCard.jsx
+++ src/Component/SideCard.jsx
@@ -5,7 +5,6 @@ import styles from "./SideCard.module.css"
 function SideCard({data:{image , title}}){
     return(
         <>
-        <h4>FAVORITE :</h4>
         <div className = {styles.card}>
             <img src={`/src/assets/${image}`} alt={title} />
             <p>{title}</p>
